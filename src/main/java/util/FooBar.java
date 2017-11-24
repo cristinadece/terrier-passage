7bb47ace2dc79e917c28e93fcba0c0188265e291
Cristina Muntean <cristina.i.muntean@gmail.com>
2017-11-24 11:05:01 +0100

    find doc id

--- src/main/java/util/FooBar.java
+++ src/main/java/util/FooBar.java
@@ -1,6 +1,10 @@
 package util;
 
+import gnu.trove.map.TObjectIntMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
+import gnu.trove.map.hash.TObjectIntHashMap;
 import org.terrier.structures.Index;
+import org.terrier.structures.IndexOnDisk;
 import org.terrier.structures.MetaIndex;
 
 import java.io.IOException;
@@ -9,10 +13,20 @@ public class FooBar {
 
     public static void main(String[] args) throws IOException {
 
-        MetaIndex metaIndex =
-                Index.createIndex().getMetaIndex();
+        IndexOnDisk index = Index.createIndex();
 
-        int docid = metaIndex.getDocument("docno", "clueweb09-enwp01-31-11362");
+        MetaIndex metaIndex = index.getMetaIndex();
+
+        int numDocs = index.getCollectionStatistics().getNumberOfDocuments();
+
+        TObjectIntMap<String> docno = new TObjectIntHashMap<String>();
+        for (int docid = 0; docid < numDocs; docid++) {
+
+            docno.put(metaIndex.getItem("docno", docid), docid);
+
+        }
+
+        int docid = docno.get("clueweb09-en0003-55-31884");
 
         System.out.printf("%d\n", docid);
 
