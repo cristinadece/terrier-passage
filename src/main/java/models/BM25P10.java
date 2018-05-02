5f0a18bec89a6db9916a2e1fcb7b6d66798848ec
Cristina Muntean <cristina.i.muntean@gmail.com>
2018-05-02 18:18:38 +0200

    Added new BM25P10, a model which splits the docs in 10 equal parts

---
+++ src/main/java/models/BM25P10.java
@@ -0,0 +1,74 @@
+package models;
+
+import org.terrier.matching.models.WeightingModel;
+import org.terrier.matching.models.WeightingModelLibrary;
+import org.terrier.structures.postings.BlockPosting;
+import org.terrier.structures.postings.Posting;
+import org.terrier.utility.ApplicationSetup;
+
+import java.util.Arrays;
+
+public class BM25P10 extends WeightingModel{
+
+    /**
+     * Splits each document in 10 equal parts
+     */
+
+    private static final long serialVersionUID = 1L;
+    private double k_1 = 1.2D;
+    private double k_3 = 8.0D;
+    private double b = 0.75D;
+    private int num_passage = 10;
+    String w = "[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]";
+    double[] weights = new double[num_passage];
+
+    public BM25P10() {
+
+        k_1 = Double.parseDouble(ApplicationSetup.getProperty("bm25p.k_1", "1.2"));
+        k_3 = Double.parseDouble(ApplicationSetup.getProperty("bm25p.k_3", "8"));
+        b = Double.parseDouble(ApplicationSetup.getProperty("bm25p.b", "0.75"));
+        w = ApplicationSetup.getProperty("bm25p.w", "[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]");
+        String[] split_w = w.replace("[","").replace("]", "").split(",");
+        weights = Arrays.stream(split_w)
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+    }
+
+    public String getInfo() {
+        return "BM25P";
+    }
+
+    public double score(double tf, double docLength) {
+        //this is plain bm25 copied from TERRIER, 'cos no code reutilisation was possible
+        double K = this.k_1 * (1.0D - this.b + this.b * docLength / this.averageDocumentLength);
+        return WeightingModelLibrary.log((this.numberOfDocuments - this.documentFrequency + 0.5D) /
+                (this.documentFrequency + 0.5D)) * ((this.k_1 + 1.0D) * tf / (K + tf)) *
+                ((this.k_3 + 1.0D) * this.keyFrequency / (this.k_3 + this.keyFrequency));
+    }
+
+    @Override
+    public double score(Posting p) {
+
+        int docLen = p.getDocumentLength();
+
+        int[] tf_passage = new int[num_passage]; //initialized to 0
+
+        int positions[] = ((BlockPosting) p).getPositions();
+        for (int i=0; i < positions.length; i++) {
+            int pos = positions[i];
+            tf_passage[pos/(docLen/num_passage)]++;
+        }
+
+        double tf = 0.0;
+        for (int i=0; i < num_passage; i++){
+            tf += weights[i]*tf_passage[i];
+        }
+
+        return score(tf, docLen);
+    }
+
+    @Override
+    public String toString() {
+        return "BM25P10";
+    }
+}
