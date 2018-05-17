bdc04d772b78e7bbf3ae41a57d7eee650d8a3889
Cristina Muntean <cristina.i.muntean@gmail.com>
2018-05-17 11:26:09 +0200

    removed prints

--- src/main/java/models/BM25P10.java
+++ src/main/java/models/BM25P10.java
@@ -43,17 +43,18 @@ public class BM25P10 extends WeightingModel{
     public double score(double tf, double docLength) {
         //this is plain bm25 copied from TERRIER, 'cos no code reutilisation was possible
         double K = this.k_1 * (1.0D - this.b + this.b * docLength / this.averageDocumentLength);
-        System.out.print((String.valueOf(tf / (K + tf))).concat("\t"));
-
-        StringBuilder ss = new StringBuilder();
-        ss.append(this.numberOfDocuments).append("|")
-                .append(this.documentFrequency).append("|")
-                .append(this.k_1).append("|")
-                .append(K).append("|")
-                .append(this.k_3).append("|")
-                .append(this.keyFrequency);
-        System.out.print(ss.toString().concat("\t"));
-
+//
+//        System.out.print((String.valueOf(tf / (K + tf))).concat("\t"));
+//
+//        StringBuilder ss = new StringBuilder();
+//        ss.append(this.numberOfDocuments).append("|")
+//                .append(this.documentFrequency).append("|")
+//                .append(this.k_1).append("|")
+//                .append(K).append("|")
+//                .append(this.k_3).append("|")
+//                .append(this.keyFrequency);
+//        System.out.print(ss.toString().concat("\t"));
+//
         return WeightingModelLibrary.log((this.numberOfDocuments - this.documentFrequency + 0.5D) /
                 (this.documentFrequency + 0.5D)) * ((this.k_1 + 1.0D) * tf / (K + tf)) *
                 ((this.k_3 + 1.0D) * this.keyFrequency / (this.k_3 + this.keyFrequency));
@@ -65,8 +66,8 @@ public class BM25P10 extends WeightingModel{
         double tf = 0.0;
         int docLen = p.getDocumentLength();
 
-        System.out.print(String.valueOf(p.getId()).concat("\t").concat(String.valueOf(docLen)).concat("\t"));
-       
+//        System.out.print(String.valueOf(p.getId()).concat("\t").concat(String.valueOf(docLen)).concat("\t"));
+
         int[] tf_passage = new int[num_passage]; //initialized to 0
 
         if (docLen > 0) {
@@ -81,11 +82,11 @@ public class BM25P10 extends WeightingModel{
             }
         }
 
-        NumberFormat formatter = new DecimalFormat("#0.00");
-        System.out.print(formatter.format(tf).concat("\t"));
-        double score1 = score(tf, docLen);
-        System.out.println(String.valueOf(score1));
-        return score1;
+//        NumberFormat formatter = new DecimalFormat("#0.00");
+//        System.out.print(formatter.format(tf).concat("\t"));
+//        double score1 = score(tf, docLen);
+//        System.out.println(String.valueOf(score1));
+        return score(tf, docLen);
     }
 
     @Override
